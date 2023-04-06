fb3aa3f7bc1df9023f288a8eeaf0aeea37624f73
Suryanshjamwal <74829495+Suryanshjamwal@users.noreply.github.com>
2023-04-06 19:59:57 +0100

    Update

---
+++ code.sql
@@ -0,0 +1,303 @@
+-- Querying data for cleaning, insights and transformation
+-- This sql file has some of the related significant queries 
+
+-----------------------------------------------------------------------
+-- Property Table
+
+-- Dropping Eircode column
+ALTER TABLE `property`
+DROP COLUMN Eircode;
+
+-- Change Currency in Excel with find and replace
+-- Replace 'Ä' to '' and then format the column price as currency
+
+-- Checking Distinct values for property description and size
+
+SELECT DISTINCT(Description_of_Property)
+FROM `property`;
+
+/*
+Output:
+Row	Description_of_Property
+1	New Dwelling house /Apartment
+2	Teach/Árasán Cónaithe Atháimhe
+3	Second-Hand Dwelling house /Apartment
+4	Teach/Árasán Cónaithe Nua
+5	Teach/?ras?n C?naithe Nua
+*/
+
+-- We need to convert Irish to English language
+
+UPDATE `property`
+SET Description_of_Property = REPLACE(Description_of_Property, 'Teach/Árasán Cónaithe Atháimhe', 'Second-Hand Dwelling house /Apartment')
+WHERE Description_of_Property='Teach/Árasán Cónaithe Atháimhe';
+
+/*
+Output:
+
+Row	Description_of_Property
+1	New Dwelling house /Apartment
+2	Second-Hand Dwelling house /Apartment
+3	Teach/Árasán Cónaithe Nua
+4	Teach/?ras?n C?naithe Nua
+*/
+
+-- Similarly we replaced the rest
+
+SELECT DISTINCT(Property_Size_Description)
+FROM `property`;
+
+/*
+Output:
+
+Row	Property_Size_Description Total
+1	níos mó ná nó cothrom le 38 méadar cearnach agus níos lú ná 125 méadar cearnach 2
+2	NULL 536210 
+3	greater than or equal to 38 sq metres and less than 125 sq metres 38085
+4	greater than 125 sq metres 6853
+5	less than 38 sq metres 3263
+6	greater than or equal to 125 sq metres 4610
+7	n?os l? n? 38 m?adar cearnach 1
+*/
+
+-- CLearly we have more than 90% NULL values in the size column
+-- Drop the Property_Size_Description
+
+ALTER TABLE `property`
+DROP COLUMN Property_Size_Description;
+
+-- Using Vlookup in excel to fill out regions based on different counties
+
+-----------------------------------------------------------------------
+-- Rental Table
+
+SELECT * FROM `rental` 
+WHERE VALUE IS NOT NULL;
+
+-- 67104 values extracted
+-- Saved the table for further cleaning
+-- Further addresses had to be separted according to different counties
+-- Excel tools like sorting, text to columns and manual transformation helped extracting county names
+-- Time taken 45 mins to clean
+
+-----------------------------------------------------------------------
+-- Population table
+-- Getting an insight 
+
+SELECT Region, Sex, Year, Age_Group, VALUE
+FROM `Population` 
+Where Year = 2021
+and Sex = 'Both sexes'
+and Region = 'State'
+
+/*
+Output:
+
+Row	Region    Sex    Year Age_Group VALUE
+1	State Both sexes 2022 All ages 5100
+
+Value is in thousands
+*/
+
+-----------------------------------------------------------------------
+-- Income Table 
+
+DELETE FROM `Income`
+WHERE Year BETWEEN 2000 AND 2009;
+
+SELECT DISTINCT(Year) 
+FROM `Income`;
+
+-- Now table has values from 2010 to 2020
+
+SELECT DISTINCT(Statistic_Label) 
+FROM `Income`;
+
+/* 
+Output:
+
+Row	Statistic_Label
+1	Primary Income
+2	Total Household Income
+3	Current Taxes on Income
+4	Income of Self Employed
+5	Total Income per Person
+6	Disposable Household Income
+7	Disposable Income per Person
+8	Index of Total Income per Person
+9	Index of Disposable Income per Person
+10	Social Benefits and Other Current Transfers
+11	Disposable Income per Person (excluding Rent)
+12	Index of Disposable Income per Person (excluding Rent)
+13	Rent of dwellings (including imputed rent of owner-occupied dwellings)
+14	Compensation of Employees (i.e. Wages and Salaries, Benefits in kind, Employers' social insurance contribution)
+*/
+
+-- For this analysis we use 'Total income per person', 'Disposable Income per Person' and 'Disposable Income per Person (excluding Rent)'
+
+-----------------------------------------------------------------------
+-- Migration table
+
+SELECT DISTINCT(Inward_or_Outward_Flow)
+FROM `Migration`
+
+/*
+Output:
+
+Row	Inward_or_Outward_Flow 
+1	Net migration 
+2	Immigrants: All origins 
+3	Emigrants: All destinations
+*/
+
+-- It would be interesting to see how emigrants and immigrants affect the housing market
+
+-----------------------------------------------------------------------
+
+-- Queries used in tableau
+
+
+SELECT AVG(Price) AS avg_price, Year, County, Description_of_Property
+FROM `property`
+GROUP BY Year, County, Description_of_Property
+ORDER BY County ASC;
+
+SELECT AVG(Price) AS avg_price, Year
+FROM `property`
+GROUP BY Year;
+
+SELECT R.Year, R.VALUE, AVG(P.Price) AS avg_price
+FROM `property` P
+INNER JOIN `GDP_R` R
+ON R.Year = P.Year
+GROUP BY R.VALUE, R.Year    
+ORDER BY R.Year ASC;
+
+SELECT * FROM `Income` 
+WHERE Statistic_Label = 'Disposable Income per Person' 
+OR Statistic_Label = 'Total Income per Person';  
+
+-- DASHBOARD 1
+
+SELECT COUNT(Price), Year, County, Description_of_property
+FROM `property` 
+WHERE Price IS NOT NULL
+GROUP BY Year, County, Description_of_property
+ORDER BY Year ASC;
+
+SELECT AVG(Price), Year, County, Description_of_property
+FROM `property` 
+WHERE Price IS NOT NULL
+GROUP BY Year, County, Description_of_property
+ORDER BY Year ASC;
+
+SELECT COUNT(r.VALUE), p.Year, p.County, r.Number_of_Bedrooms
+FROM `property` p
+LEFT JOIN `rental` r
+ON p.County = r.Location
+GROUP BY p.Year, p.County, r.Number_of_Bedrooms
+ORDER BY p.Year ASC;
+
+SELECT AVG(r.VALUE), p.Year, p.County, r.Number_of_Bedrooms
+FROM `property` p
+INNER JOIN `rental` r
+ON p.County = r.Location
+GROUP BY p.Year, p.County, r.Number_of_Bedrooms
+ORDER BY p.Year ASC;
+
+-- DASHBOARD 2
+
+SELECT DISTINCT(g.VALUE)*1000000 AS GVA0, g.Year, AVG(p.Price)
+FROM `property` p
+INNER JOIN `GVA` g
+ON p.Year = g.Year
+GROUP BY g.Year, g.VALUE
+ORDER BY g.Year ASC;
+
+SELECT i.Percent_increase, p.Year, AVG(p.Price) AS AveragePrice
+FROM `property` p
+RIGHT JOIN `inflation`i
+ON p.Year = i.Year
+GROUP BY p.Year, i.Percent_increase
+ORDER BY p.Year ASC;
+
+SELECT i.interest_rates, p.Year, AVG(p.Price) AS AveragePrice
+FROM `property` p
+INNER JOIN `interest`i
+ON p.Year = i.Year
+GROUP BY p.Year, i.interest_rates
+ORDER BY p.Year ASC;
+
+SELECT SUM(m.VALUE) OVER(PARTITION BY m.Inward_or_Outward_Flow) AS Migrants,
+p.Year, AVG(p.Price) AS AveragePrice, m.Inward_or_Outward_Flow
+FROM `property` p
+INNER JOIN `migration`m
+ON p.Year = m.Year
+GROUP BY p.Year, m.Inward_or_Outward_Flow
+ORDER BY p.Year ASC;
+
+SELECT u.Annual_change, p.Year, AVG(p.Price) AS AveragePrice
+FROM `property` p
+INNER JOIN `uemployement` u
+ON p.Year = u.Year
+GROUP BY p.Year, u.Annual_change
+ORDER BY p.Year ASC;
+
+-- DASHBOARD 3
+
+SELECT VALUE, Year, Property_Type, Number_of_Bedrooms
+FROM `rental` 
+WHERE Property_Type 
+IN ('Apartment', 'Detached House', 'Semi-Detached House', 'Terrace House');
+
+SELECT AVG(VALUE), Year, Property_Type, Number_of_Bedrooms, Location, 
+FROM `rental` 
+WHERE Property_Type 
+IN ('Apartment', 'Detached House', 'Semi-Detached House', 'Terrace House')
+GROUP BY Property_Type, Location
+ORDER BY Property_Type ASC;
+
+-- DASHBOARD 4
+
+SELECT COUNT(VALUE), Stamp_Duty_Event,
+Year, Transaction_Class, NUTS_3_Region
+FROM `Buyers` 
+WHERE St = 'Volume of Sales'
+Group BY Year, NUTS_3_Region
+ORDER BY Year DESC;
+
+SELECT AVG(VALUE), Stamp_Duty_Event,
+Year, Transaction_Class, NUTS_3_Region
+FROM `Buyers` 
+WHERE St = 'Median Price'
+Group BY Year, NUTS_3_Region
+ORDER BY Year DESC;
+
+SELECT AVG(VALUE), Stamp_Duty_Event,
+Year, Transaction_Class, NUTS_3_Region, Type_of_Buyer
+FROM `Buyers` 
+WHERE St = 'Median Age'
+Group BY Year, NUTS_3_Region
+ORDER BY Year DESC;
+
+SELECT AVG(VALUE), Stamp_Duty_Event,
+Year, Transaction_Class, NUTS_3_Region, Type_of_Buyer
+FROM `Buyers` 
+WHERE St = 'Median Income'
+Group BY Year, NUTS_3_Region
+ORDER BY Year DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Exploratory Data Analysis: Real Estate Market Trends in Ireland (2010-2021)
+
+<table align = "center">
+  <tr>
+    <td><img src="pic.png"  width="200" height="250" />
+ </table>
+
+This project is an exploratory data analysis of the real estate market trends in Ireland from the year 2010 to 2021. The purpose of this analysis is to provide insights into the performance of the Irish real estate market over this period, and to identify any notable trends or patterns that may be of interest to potential buyers, investors, or other stakeholders in the industry.
+
+## Data Source
+
+The data used for this analysis was sourced from Central Statistics Office (CSO), one of the largest online statistics platforms in Ireland, gov.ie, property price register and EU Central Bank. 
+
+The dataset pulled from property register includes information on around 600,000 property listings over the period from January 2010 to December 2023. Rest other datasets from CSO and EU Central Bank include data from 2010-2021 of Rental properties, Buyers demographics, Gross Value added, population, migration, unemployement rate, interest rates, income and inflation rate. 
+
+These datasets were chosen to find out how each one of the decision metrics inlfuenced property and rental prices in the Irish market.
+
+## Data Exploration
+
+The data was first cleaned and preprocessed to ensure that it was ready for analysis. This involved removing any duplicate entries, handling missing values, and converting the data into a more usable format.
+
+Once the data was cleaned, a series of exploratory data analysis (EDA) techniques were used to examine the trends and patterns in the data. This included the use of descriptive statistics, data visualization, and time-series analysis.
+
+For this project I used SQL and Excel for exploring all the data in hand.
+
+## Data Cleaning and Transformation
+
+Using SQL and Excel, the datasets were queried to find out any abnormalities or duplicate values. 
+
+### Some key relevant Excel transformations used
+
+* Fetching County names from a variety of Addresses
+* Left and right formula transformation
+* Adding year column with Year Funtion
+* TRIM values 
+* Using VLOOKUP funtion to add Region field based on County names
+* Correcting Datatypes
+* Deleting irrelevant fields and rows
+* Currency modifications 
+* Translating Irish phrases to English
+* Excluding data before 2010
+* INDEX and FILTER data
+* Concatenate relevant datasheets 
+* Find and replace values
+
+### SQL Querying 
+
+Using the query language most of the transforamtions, extraction and manipulations were handled.
+
+This [sql file](code.sql) contains some of the key relevant queries used for this analysis project. SQL commands like JOIN ON, UPDATE, PARTITION BY, GROUP BY, ORDER BY, DELETE, etc helped in general manipulation tasks.
+
+## Data Visualisation 
+
+<div class='tableauPlaceholder' id='viz1680807121880' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;RealEstateAnalysis-Ireland&#47;Summary&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='RealEstateAnalysis-Ireland&#47;Summary' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;RealEstateAnalysis-Ireland&#47;Summary&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1680807121880');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.maxWidth='1450px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';vizElement.style.maxHeight='910px';} else { vizElement.style.width='100%';vizElement.style.height='2150px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+
